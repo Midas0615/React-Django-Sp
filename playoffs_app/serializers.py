90bcaface920f6c6c7a4c956a2e3514bc6d81be1
Midas0615 <b4251154@gmail.com>
2019-06-08 22:32:40 -0400

    Added serializers

---
+++ playoffs_app/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import League, Team, Player
+
+class LeagueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = League
+        fields = ('id', 'leagueName', 'league_logo_url', 'sport', 'artist')
+    
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ('id', 'teamCity', 'teamName', 'team_logo_url', 'sport', 'league', 'conference', 'division', 'championships', 'wins', 'losses', 'standing', 'favorite')
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ('id', 'playerName', 'player_photo_url', 'sport', 'league', 'position', 'favorite')
