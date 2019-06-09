ba7375e8d4ce37d190bd7c09bc67c5d87cc1f2ba
Midas0615 <b4251154@gmail.com>
2019-06-08 22:48:29 -0400

    Added app urls and views

--- playoffs_app/urls.py
+++ playoffs_app/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('leagues', views.LeagueView)
+router.register('teams', views.TeamView)
+router.register('players', views.PlayerView)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- playoffs_app/views.py
+++ playoffs_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import LeagueSerializer, TeamSerializer, PlayerSerializer
+from .models import League, Team, Player
+
+
+class LeagueView(viewsets.ModelViewSet):
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+
+class TeamView(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class PlayerView(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
