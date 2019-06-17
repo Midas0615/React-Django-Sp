f74f316e19ec19360e7ed9a0912fa3f515e777a3
Midas0615 <b4251154@gmail.com>
2019-06-17 11:56:20 -0400

    Updated urls.py files to resolve browser reload error

--- playoffs_project/urls.py
+++ playoffs_project/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('playoffs_app.urls')),
-    path('/*', views.FrontendAppView.as_view()) #New URL for the index route
+    re_path(r'^.*$', views.FrontendAppView.as_view()) #New URL for the index route
 ]
