9cd4191868097be11a54fad5881aee58cb173ab2
Midas0615 <b4251154@gmail.com>
2019-06-17 12:52:32 -0400

    Testing paths in urls.py

--- playoffs_project/urls.py
+++ playoffs_project/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.FrontendAppView.as_view()), #New URL for the index route
     path('api/v1/', include('playoffs_app.urls')),
-    re_path(r'^.*$', views.FrontendAppView.as_view()) #New URL for the index route
+    # re_path(r'^.*$', views.FrontendAppView.as_view()) #New URL for the index route
 ]
